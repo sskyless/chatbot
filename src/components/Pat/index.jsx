b0a0a7276ea816857552396adcf467ee13439884
liguangxin <965322027@qq.com>
2021-06-04 21:45:31 +0800

    版本V1.0

---
+++ src/components/Pat/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Pat extends Component {
+    render() {
+        return (
+            <div>
+                我拍了拍'M besos' 的小脸说:真好看
+            </div>
+        );
+    }
+}
+
+export default Pat;
---
+++ src/components/Withdraw/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Withdraw extends Component {
+    render() {
+        return (
+            <div>
+                'M besos' 撤回了一条消息
+            </div>
+        );
+    }
+}
+
+export default Withdraw;
